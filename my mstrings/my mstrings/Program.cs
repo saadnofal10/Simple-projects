d77ed207ed91996dfa8f479be153d6010271c034
saadnofal10 <sarg10nofal10@gmail.com>
2025-06-02 12:35:01 +0300

    Add files via upload

---
+++ my mstrings/my mstrings/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace assci
+{
+
+    internal class Program
+    {
+        class MyString
+        {
+            string sent;
+
+            public MyString() { }
+            public MyString(string sent) { this.sent = sent; }
+            public static MyString operator -(MyString s, int n)
+            {
+                MyString t = new MyString();
+                if (s.sent.Length <= n) { return t; }
+                for (int i = 0; i < s.sent.Length - n; i++)
+                {
+                    t.sent += s.sent[i];
+                }
+                return t;
+            }// remove last n letter:
+            public static MyString operator -(MyString s, char a)
+            {
+                MyString t = new MyString();
+                for (int i = 0; i < s.sent.Length; i++)
+                {
+                    if (s.sent[i] != a)
+                    {
+                        t.sent += s.sent[i];
+                    }
+
+                }
+                return t;
+            }//remove letter that u select:
+            public static MyString operator +(MyString s, char a)
+            {
+                MyString t = new MyString(s.sent);
+
+                if (s.sent.Length <= 0) { return s; }
+
+                t.sent += a;
+
+
+                return t;
+            }// add letter that u selcet:
+
+            public static MyString operator ++(MyString s)
+            {
+                if (s.sent.Length <= 0) { return s; }
+                MyString t = new MyString(s.sent);
+                // (add a letter from s to t then give me t) (-1: last char of sentence)
+                t.sent += s.sent[s.sent.Length - 1];
+                return t;
+            }// add last letter
+            public static MyString operator --(MyString s)
+            {
+                if (s.sent.Length <= 0) { return s; }
+                MyString t = new MyString();//s.sent=====>هي الكلمة الرئيسية
+                for (int i = 0; i < s.sent.Length - 1; i++) { t.sent += s.sent[i]; }//i: index of letters// s.sent: main word// t.sent: temprary word// 
+
+                return t;
+            }// remove last letter
+
+            public override string ToString()
+            {
+                return sent;
+            }//print
+        }
+        class Mystring
+        {
+            char[] text;
+            public Mystring() { }
+            public Mystring(string temp)
+            {
+                text = new char[temp.Length];
+                for (int i = 0; i < temp.Length; i++) { text[i] = temp[i]; }
+            }
+            public static Mystring operator --(Mystring s)
+            {
+                Mystring t = new Mystring();
+                t.text = new char[s.text.Length - 1];
+                for (int i = 0; i < t.text.Length; i++)
+                {
+                    t.text[i] = s.text[i];
+                }
+                return t;
+            }
+            public void print() { Console.WriteLine(text); }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            MyString s = new MyString("saad");
+            s = s + 'a';
+
+            Console.WriteLine(s);
+            Mystring s2 = new Mystring("saad");
+            s2--;
+            s2.print();
+        }
+    }
+}
---
+++ my mstrings/my mstrings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("my mstrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("my mstrings")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e1397b82-028d-4fec-9720-0e6772bdacf3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
